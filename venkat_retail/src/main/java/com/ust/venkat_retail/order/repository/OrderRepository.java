e3ac6ee9282713fc2b7d907e43053b615122fc80
yaswanth <307366@ust.com>
2026-04-10 10:27:51 +0530

    latest

--- venkat_retail/src/main/java/com/ust/venkat_retail/order/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ust.venkat_retail.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ust.venkat_retail.order.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {}
